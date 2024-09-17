b7f2f50d1e13f0bf7c4e010c0389d97517fb087b
macbook <macbook@macbooks-MacBook-Pro.local>
2024-09-17 17:23:12 +0700

    update image1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,13 @@
   <div id="app">
     <!-- <h1>Login with LINE and Send Message</h1>
     <h3>LINE User ID: {{ userId }}</h3> -->
-    <img v-if="_profilePictureUrl" :src="_profilePictureUrl" alt="Profile Image" width="100" />
+    <!-- <img v-if="userId" :src="imgShow" alt="Shop Image" width="300" />
+    <img v-if="userId" :src="imgBanner" alt="Shop Image" width="300" /> -->
+    <img :src="imgShow" alt="Shop Image" width="300" />
+    <img :src="imgBanner" alt="Shop Image" width="300" />
+    <!-- <img v-if="_profilePictureUrl" :src="_profilePictureUrl" alt="Profile Image" width="100" /> -->
     <button v-if="!userId" @click="loginWithLINE" class="button">Login with LINE</button>
-    <button v-if="userId" @click="openLine" class="button">Open LINE</button>
+    <button v-if="userId" @click="openLine" class="button">LINE CHAT</button>
     <!-- <button v-if="userId" @click="sendMessage" class="button">Send Message to LINE Chat</button> -->
     <button v-if="userId" @click="logout" class="button">Logout</button>
   </div>
@@ -16,6 +20,8 @@ import liff from '@line/liff'
 export default {
   data() {
     return {
+      imgShow: 'https://www.doctorgarn.com/wp-content/uploads/2024/01/bg4-03.png',
+      imgBanner: 'https://www.doctorgarn.com/wp-content/uploads/2024/01/font-2.png',
       _profile: {},
       _profilePictureUrl: '',
       userId: null,
